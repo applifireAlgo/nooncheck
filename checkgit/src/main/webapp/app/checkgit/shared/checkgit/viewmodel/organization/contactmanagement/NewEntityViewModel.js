6f2f781d23af282d8d5e6119b607430e339cdc7e
root <anant@algorhythm.co.in>
2016-05-24 14:09:18 +0000

    File deleting

--- checkgit/src/main/webapp/app/checkgit/shared/checkgit/viewmodel/organization/contactmanagement/NewEntityViewModel.js
+++
@@ -1,6 +0,0 @@
-Ext.define('Checkgit.checkgit.shared.checkgit.viewmodel.organization.contactmanagement.NewEntityViewModel', {
-     "extend": "Ext.app.ViewModel",
-     "alias": "viewmodel.NewEntityViewModel",
-     "model": "NewEntityModel",
-     "data": {}
-});
--- checkgit/src/main/webapp/app/checkgit/shared/checkgit/viewmodel/organization/locationmanagement/CheckingViewModel.js
+++
@@ -1,6 +0,0 @@
-Ext.define('Checkgit.checkgit.shared.checkgit.viewmodel.organization.locationmanagement.CheckingViewModel', {
-     "extend": "Ext.app.ViewModel",
-     "alias": "viewmodel.CheckingViewModel",
-     "model": "CheckingModel",
-     "data": {}
-});
